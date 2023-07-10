ffee65483fa890773274da82f2fe5ae0ddcc04e4
Bruno Rafael dos Santos Gomes <108811099+Bruno735@users.noreply.github.com>
2023-07-10 12:34:15 -0300

    Add files via upload

---
+++ Prova-node/index.js
@@ -0,0 +1,43 @@
+const http = require('http')
+const url = require('url')
+const os = require('os')
+
+
+
+// const PORT = 8000
+
+// Atividade 1
+
+// const server = http.createServer((req, res)=>{
+//     res.setHeader('Content-Type', 'applicantion/json' )
+//         const objeto = [
+//             { id: 1, nome: 'Carlos'},
+//             { id: 2, nome: 'Bruno'},
+//             { id: 3, nome: 'Leticia'},
+//         ]
+
+//         const json = JSON.stringify(objeto)
+//         console.log(json)
+// })
+
+//     server.listen(PORT, () =>{
+//         console.log('servidor on ✅')
+//     })
+
+//Atividade 2
+
+// const server = http.createServer((req, res)=>{
+//     const myURL = new                               
+//     URL(`https://.al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf`); 
+//     if((req.url)
+
+//     console.log(myURL.href); 
+//     console.log(myURL.pathname);
+// })
+
+
+//Atividade 5 
+
+// console.log(os.cpus())
+
+
